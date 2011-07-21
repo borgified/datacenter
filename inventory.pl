cb858acdd472b0246d136fc448ceb55c3f087bce
Fwiffo <borgified@gmail.com>
2011-07-21 16:24:23 -0700

    added backup column

--- inventory.pl
+++ inventory.pl
@@ -7,7 +7,7 @@ my $dbh = DBI->connect('DBI:mysql:datacenter', 'root', ''
 	           ) || die "Could not connect to database: $DBI::errstr";
 
 
-my $sth=$dbh->prepare('select id,hostname,asset_tag,make,model,serial_number,cab,position,dept from rwc order by cab, (position+0) asc');
+my $sth=$dbh->prepare('select id,hostname,asset_tag,make,model,serial_number,cab,position,dept,backup from rwc order by cab, (position+0) asc');
 $sth->execute();
 
 print header;
@@ -31,17 +31,17 @@ EOF
 
 
 print "<table border='1'>";
-print "<tr><th>X</th><th>U</th><th>I</th><th>hostname</th><th>asset_tag</th><th>make</th><th>model</th><th>serial_number</th><th>cab</th><th>position</th><th>dept</th></tr>";
+print "<tr><th>X</th><th>U</th><th>I</th><th>hostname</th><th>asset_tag</th><th>make</th><th>model</th><th>serial_number</th><th>cab</th><th>position</th><th>dept</th><th>backup</th></tr>";
 
 my $currcab="";
 
 while(my @line=$sth->fetchrow_array()){
 	my $id=shift(@line);
-	my($hostname,$asset_tag,$make,$model,$serial_number,$cab,$position,$dept)=@line;
+	my($hostname,$asset_tag,$make,$model,$serial_number,$cab,$position,$dept,$backup)=@line;
 
 	if($currcab ne $cab){
 print <<EOF;
-<tr><td></td><td></td><td></td><td colspan='8'><a href="inventory.pl?insert=$cab.$position"><hr style='margin:0'></a></td></tr>
+<tr><td></td><td></td><td></td><td colspan='9'><a href="inventory.pl?insert=$cab.$position"><hr style='margin:0'></a></td></tr>
 EOF
 	}
 
@@ -53,7 +53,7 @@ print <<EOF;
 <td><a href="insert_above.pl?position=$position&cab=$cab">&uarr;</a>&nbsp;<a href="insert_below.pl?position=$position&cab=$cab">&darr;</a></td>
 EOF
 	my $i=0;
-	my @inputname = qw/hostname asset_tag make model serial_number cab position dept/;
+	my @inputname = qw/hostname asset_tag make model serial_number cab position dept backup/;
 	foreach my $item (@line){
 		print "<td><input type='text' name='$inputname[$i].$id' value='$item'></td>";
 		$i++;
